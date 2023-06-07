c91154a4dd3c84a9410a6837f70477bb1110e6d2
Mohd Anas <mohdanas9595@gmail.com>
2023-06-07 21:22:54 +0530

    fixed Logout

--- controllers/user.js
+++ controllers/user.js
@@ -47,8 +47,8 @@ export const getMyProfile = (req, res) => {
 export const logout = (req, res) => {
     return res.status(200).cookie("token", "", {
         expires: new Date(Date.now()),
-        sameSite: process.env.NODE_ENV = "Developement" ? "lax" : "none",
-        secure: process.env.NODE_ENV = "Developement" ? false : true,
+        sameSite: process.env.NODE_ENV == "Developement" ? "lax" : "none",
+        secure: process.env.NODE_ENV == "Developement" ? false : true,
 
     }).json({
         success: true,
